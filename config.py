8ca63cca2c3a235eec90f219f466b9155f54f6be
Antony-al <antonaleshkin32rus@gmail.com>
2024-11-17 20:02:06 +0300

    test commit

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ review_chat_id = config["review_chat_id"]
 channels_to_monitor = config["channels_to_monitor"]
 my_channel = config["my_channel"]
 my_channel_id = config["my_channel_id"]
+
 db_name = 'meme_bot.db'
 ad_keywords = ["реклама", "спонсор", "поддержи", "подписывайся", "партнёрская", "промо"]
 link_pattern = re.compile(r"(t\.me/|@|https?://)")
