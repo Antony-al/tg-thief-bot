bed9eea613939c0b5de4c408b7d13d48519c520f
Anton <bcp.ant@gmail.com>
2024-11-18 14:21:19 +0300

    started

---
+++ bot.py
@@ -0,0 +1,4 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from handlers import cmd_handlers, txt_handlers, handlers_func, callback_handlers
+
--- handlers/cmd_handlers.py
+++ handlers/cmd_handlers.py
@@ -1,15 +1,1 @@
-from aiogram import Router
-from aiogram.filters import Command
-from aiogram.types import Message
-
-router = Router()
-bot = None
-
-def set_bot(instance):
-    global bot
-    bot = instance
-
-
-@router.message(Command("start"))
-async def send_welcome(message: Message):
-    await message.answer("Привет! Этот бот проверяет и публикует мемы. Используйте команды для управления.")
+pass
