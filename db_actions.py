21b55a3a3a8576cb96f64646660e768eab3f25d2
Anton <bcp.ant@gmail.com>
2024-11-18 14:52:35 +0300

    startedx2

--- db_actions.py
+++ db_actions.py
@@ -1,1 +1,14 @@
-pass
+import sqlite3
+db_path = 'data/botdb.db'
+
+class MemeStealerDb():
+    def __init__(self):
+        self.conn = sqlite3.connect(db_path)
+        self.cursor = self.conn.cursor()
+
+    def close(self):
+        self.conn.close()
+
+    def create_db(self):
+            
+                  
