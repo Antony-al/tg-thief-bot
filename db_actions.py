fa0c8948749c4ac3cee86303d4f154727bec9d71
Anton <bcp.ant@gmail.com>
2024-11-19 15:18:17 +0300

    some db procedures

--- db_actions.py
+++ db_actions.py
@@ -18,13 +18,34 @@ class MemeStealerDb():
             )
         """)
         self.cursor.execute("""
-            CREATE TABLE IF NOT EXISTS posts (
+            CREATE TABLE IF NOT EXISTS queue (
                 channel_id TEXT,
                 message_id INTEGER,
+                media_path TEXT,
+                caption TEXT,
                 PRIMARY KEY (channel_id, message_id)
             )
         """)
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                usertgid INTEGER,
+                api_id INTEGER DEFAULT 0,
+                api_hash TEXT DEFAULT "",
+                PRIMARY KEY (usertgid)
+            )
+        """)
         return self.conn.commit() 
+    
+    def user_exists(self, user_id):
+        pass
+
+    def add_user(self, user_id):
+        pass
+
 
     def set_tg_client(self):
-        pass        
+        pass
+
+    def get_tg_client(self):
+        pass
+        
