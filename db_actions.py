096a8db799f622e79f3d18a098264133905da706
Anton <bcp.ant@gmail.com>
2024-11-19 18:30:30 +0300

    db functions

--- db_actions.py
+++ db_actions.py
@@ -37,20 +37,33 @@ class MemeStealerDb():
         """)
         return self.conn.commit() 
     
-    def user_exists(self, user_id):
-        result = self.cursor.execute("SELECT `usertgid` FROM `users` WHERE `usertgid` = ?", (user_id,))
+    def user_exists(self, usertgid):
+        result = self.cursor.execute("SELECT `usertgid` FROM `users` WHERE `usertgid` = ?", (usertgid,))
         return bool(len(result.fetchall()))
 
-    def add_user(self, user_id):
-        self.cursor.execute("INSERT INTO `users` (`usertgid`) VALUES (?)", (user_id,))
+    def add_user(self, usertgid):
+        self.cursor.execute("INSERT INTO `users` (`usertgid`) VALUES (?)", (usertgid,))
         return self.conn.commit()
 
 
-    def set_tg_client(self):
-        pass
+    def set_tg_client(self, usertgid, api_id, api_hash):
+        self.cursor.execute("INSERT INTO users (usertgid, api_id, api_hash) VALUES (?, ?, ?)", (usertgid, api_id, api_hash,))
 
-    def get_tg_client(self):
-        pass
+    def get_tg_client(self, usertgid):
+        result = []
+        self.cursor.execute("SELECT api_id, api_hash FROM users WHERE usertgid = ?", (usertgid,))
+        result = self.cursor.fetchone()
+        return result
 
+                                    ###Проверить в дейтсвии надо будет###
+                                    
     def add_channel(self, user_id, channel):
-        pass    
+        self.cursor.execute("SELECT channels_to_monitor FROM users WHERE usertgid = ?", (user_id,))
+        result = self.cursor.fetchone()
+        if result:
+            current_channels = result[0]
+            if channel not in current_channels.split('|'):
+                self.cursor.execute("UPDATE users SET channels_to_monitor = ? WHERE usertgid = ?", (channel, user_id))
+                self.conn.commit()
+            else:
+                print(f"Канал {channel} уже существует для usertgid {user_id}.")
