fff64f306812519866e51e7b9c7bbf9c94659bb0
Anton <bcp.ant@gmail.com>
2024-11-19 15:26:18 +0300

    config_reader

--- config_reader.py
+++ config_reader.py
@@ -3,6 +3,8 @@ from pydantic import SecretStr
 
 class Settings(BaseSettings):
     bot_token: SecretStr
+    my_channel: str
+    my_channel_id: str
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
 
