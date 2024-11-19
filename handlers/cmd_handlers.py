ba6e6afa8ca597e3ef7ad147e18ca853ef62159e
Anton <bcp.ant@gmail.com>
2024-11-19 18:55:28 +0300

    start command

--- handlers/cmd_handlers.py
+++ handlers/cmd_handlers.py
@@ -1,1 +1,16 @@
-pass
+from aiogram import Router, F
+from aiogram.filters import Command
+from aiogram.types import Message
+import db_actions
+import handlers.hadlers_reply as reply
+
+router = Router()
+
+@router.message(Command("start")) 
+async def cmd_start(message: Message):
+    db = db_actions.MemeStealerDb()
+    if not db.user_exists(message.from_user.id):
+        db.add_user(message.from_user.id)
+        db.close()
+    await message.answer(reply.start_reply())
+
